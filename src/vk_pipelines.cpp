ff85354a6d1c5b19b4ee0bb9be4272b8bff3c44c
Porrith Suong <porrith.suong@gmail.com>
2025-03-07 12:07:51 -0500

    Reads the spirv shader and loads it into memory.

--- src/vk_pipelines.cpp
+++ src/vk_pipelines.cpp
@@ -1,1 +1,42 @@
-﻿#include <vk_pipelines.h>
+﻿#include <vector>
+#include <vk_pipelines.h>
+#include <fstream>
+#include <vk_initializers.h>
+#include <vulkan/vulkan_core.h>
+
+bool vkutil::load_shader_module(const char *filePath, VkDevice device, VkShaderModule *outShaderModule) {
+    // Open the file with the ptr at the end
+    std::ifstream file(filePath, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    // Find what the size of the file is by looking up the location of the ptr.
+    size_t fileSize = (size_t)file.tellg();
+
+    // SpirV expects the buffer to be in uint32 type
+    std::vector<uint32_t> buffer(fileSize / sizeof(uint32_t));
+
+    // Reset the ptr to the head
+    file.seekg(0);
+    file.read((char*)buffer.data(), fileSize);
+
+    // Close the file
+    file.close();
+
+    // Create the shader module
+    VkShaderModuleCreateInfo createInfo = {
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .pNext = nullptr, 
+        .codeSize = buffer.size() * sizeof(uint32_t), // In bytes
+        .pCode = buffer.data(), // Actual code
+    };
+
+    VkShaderModule shaderModule;
+    if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+        return false;
+    }
+    *outShaderModule = shaderModule;
+    return true;
+}
