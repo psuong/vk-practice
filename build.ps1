37db1fef34cfb8a1da0c6cfb26172cb03ba8da76
Porrith Suong <porrith.suong@gmail.com>
2025-03-08 18:26:34 -0500

    Modifies the pipeline to build slang files.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+param (
+    [Parameter(Mandatory=$true)][string]$type = "slang"
+)
+
 if (-Not (Test-Path -Path bin)) {
     New-Item .\bin -ItemType "directory"
 }
@@ -9,10 +13,22 @@ if (-Not (Test-Path -Path bin/shaders)) {
 cmake --preset ninja
 cmake --build --preset ninja-build
 
-foreach ($shader in Get-ChildItem .\shaders -Filter *.comp) {
-    $name = (Get-Item $shader).BaseName + ".spv"
-    $target = ".\bin\shaders\$name"
+if ($type -eq "glsl") {
+    Write-Host "Compiling glsl shaders"
+    foreach ($shader in Get-ChildItem .\shaders -Filter *.comp) {
+        $name = (Get-Item $shader).BaseName + ".spv"
+        $target = ".\bin\shaders\$name"
 
-    Write-Host "Compiling $shader"
-    glslangValidator.exe -V $shader -o $target
+        Write-Host "Compiling $shader"
+        glslangValidator.exe -V $shader -o $target
+    }
+} else {
+    Write-Host "Compiling slang shaders"
+    foreach ($shader in Get-ChildItem .\shaders -Filter *.slang) {
+        $name = (Get-Item $shader).BaseName + ".spv"
+        $target = ".\bin\shaders\$name"
+
+        Write-Host "Compiling $shader"
+        slangc $shader -target spirv -o $target
+    }
 }
